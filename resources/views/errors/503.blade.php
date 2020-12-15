c10299ba69faea560cde6252eb8139b4d566a858
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2020-12-16 00:16:56 +0100

    Added closing maintenance status page.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<title>Site Maintenance</title>
+<title>Momentan keine Arbeitsplatzbuchung möglich</title>
 <style>
     body { text-align: center; padding: 150px; }
     h1 { font-size: 50px; }
@@ -10,9 +10,17 @@
 </style>
 
 <article>
-    <h1>We&rsquo;ll be back soon!</h1>
+    <h1>Momentan keine Arbeitsplatzbuchung möglich</h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
+        <p>Sehr geehrte Nutzer*innen,</p>
+        <p>die Senatskanzlei Wissenschaft und Forschung und die Hochschulen haben beschlossen, dass ab Mittwoch, den 16.12.2020 wissenschaftliche Bibliotheken nur Online-Dienste 
+        und Leihbetrieb anbieten dürfen. D.h. in den Bibliotheken der TU und der UdK Berlin ist ab Mittwoch, den 16.12.2020 bis vorläufig 10.1.2021 die Nutzung der Arbeitsplätze 
+        nicht mehr gestattet. Das gilt auch für bereits gebuchte Plätze in diesem Zeitraum. Die Ausleihe ist während der Öffnungszeiten der Bibliotheken weiterhin möglich.</p>
+        <p>Weitere Informationen finden Sie zeitnah auf der <a href="https://ub.tu-berlin.de/">Website der Universitätsbibliothek der TU Berlin</a> bzw. auf der 
+        <a href="https://www.udk-berlin.de/service/universitaetsbibliothek/">Website der Universitätsbibliothek der UdK Berlin</a>.</p>
+        <p>Vom 28.-30.12. sind die Bibliotheken an der TU Berlin und UdK Berlin für den Publikumsverkehr geschlossen. Bitte beachten Sie für die Ausleihe sowie das Abholen von 
+        Bestellungen die Öffnungszeiten der jeweiligen Bibliothek ab dem 16.12.2020.</p>
+        <p>Wir wünschen Ihnen trotz aller Widrigkeiten schöne Feiertage und kommen Sie gut ins neue Jahr.</p>
+        <p>Ihr Bibliotheksteam</p>
     </div>
 </article>
